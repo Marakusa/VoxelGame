f264e5b592a8728327fc0204740893887ad18df6
Markus <m.kannisto01@gmail.com>
2021-11-28 15:25:43 +0200

    Update Renderer.cs

--- VoxelGame/Engine/Renderer.cs
+++ VoxelGame/Engine/Renderer.cs
@@ -2,6 +2,7 @@ namespace VoxelGame.Engine
 {
     public class Renderer
     {
+        // TODO: Use this instead of calling these in Game class
         public void Draw(VertexBuffer va, IndexBuffer ib, Shader shader)
         {
             shader.Bind();
